33620fbbab875c3882e11e6e0d997b00b79e2911
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-04-11 10:12:27 -0500

    unhide agau to landing page

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const addApps = [
       {
         order: 2,
         thumbnail: "./img/residuos/AGAU_CARD.png",
-        is_show: false,
+        is_show: true,
         title: "./img/residuos/AGAU_TITLE.png",
         cover: "./img/residuos/AGAU_IMAGE.png",
         description:
